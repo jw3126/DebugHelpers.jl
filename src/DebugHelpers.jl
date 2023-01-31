639cc10e4bbb33cafcd0a09e46ce48e2247b2ccb
Jan Weidner <jw3126@gmail.com>
2023-01-31 11:30:13 +0100

    init

--- src/DebugHelpers.jl
+++ src/DebugHelpers.jl
@@ -1,5 +1,26 @@
 module DebugHelpers
+export @into_global_scope
 
-# Write your package code here.
+"""
+
+    @into_scope(d::AbstractDict{Symbol})
+
+Turns each `key,val` pair in `d` into a variable assignment.
+"""
+macro into_scope(d)
+    d = __module__.eval(d)
+    ret = quote end
+    for (key, val) in pairs(d)
+        if !(key isa Symbol)
+            msg = """Keys should be symbols. Got:
+            typeof(key) = $(typeof(key))
+            key = $key
+            """
+            error(msg)
+        end
+        push!(ret.args, :($(esc(key)) = $val))
+    end
+    ret
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,23 @@
-using DebugHelpers
+using DebugHelpers: @into_scope
 using Test
 
-@testset "DebugHelpers.jl" begin
-    # Write your tests here.
+safehouse = [
+    Dict(:a=>1, :b=>2),
+    Dict(:a=>10, :b=>20, :c=>30),
+]
+@testset "@into_scope" begin
+    @test !isdefined(@__MODULE__, :a)
+    @test !isdefined(@__MODULE__, :b)
+    @test !isdefined(@__MODULE__, :c)
+    @into_scope safehouse[1]
+    @test a == 1
+    @test b == 2
+    @test !isdefined(@__MODULE__, :c)
+    @into_scope safehouse[2]
+    @test a == 10
+    @test b == 20
+    @test c == 30
 end
+@test !isdefined(@__MODULE__, :a)
+@test !isdefined(@__MODULE__, :b)
+@test !isdefined(@__MODULE__, :c)
